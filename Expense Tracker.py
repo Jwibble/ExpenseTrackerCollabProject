c9224f388a465794d2207236404659e163dd4a61
kliu42 <katrina.liu773@gmail.com>
2025-02-16 20:54:20 -0800

    commit to main

--- Expense Tracker.py
+++ Expense Tracker.py
@@ -1,44 +1,52 @@
 #Katrina Liu and Eric Yang's Expense Tracker Project
 import datetime
 
-list_expense = []
+expense_dict = {}
 
-menu = input("Welcome to your expense tracker. Would you like to: \n"
-                "1. Add New Expense \n"
-                "2. View all expenses \n"
-                "3. Categorize expenses \n"
-                "4. Calaculate total expenses \n"
-                "5. Delete expenses \n"
-                "Please select which option by the number: ")
+print('Welcome to your expense tracker.')
 
-def Expense_Addition():
-    if menu == '1':
-        category =  input("Please enter the categogry for this expense: ").lower().strip().title()
-        amount = float(input("Enter the amount for this expense: "))
-        date = input("Please enter the date for when this expense is due in YYYY/MM/DD format: ")
+while True:
 
-Expense_Addition()
+    menu = input("Would you like to: \n"
+                    "1. Add New Expense \n"
+                    "2. View all expenses \n"
+                    "3. Categorize expenses \n"
+                    "4. Calaculate total expenses \n"
+                    "5. Delete expenses \n"
+                    "6. Exit the app \n"
+                    "Please select which option by the number: ")
 
-def Expense_View():
-    if menu == '2':
-    test2 test2
+    def Expense_Addition():
+        if menu == '1':
+            category =  input("Please enter the categogry for this expense: ").lower().strip().title()
+            amount = float(input("Enter the amount for this expense: "))
+            date = input("Please enter the date for when this expense is due in YYYY/MM/DD format: ")
 
-Expense_Addition()
+    Expense_Addition()
 
-def Categorize_Expenses():
-    if menu == '3':
-    yada yada
+    def Expense_View():
+        if menu == '2':
 
-Categorize_Expenses()
+            print("Here is the list of all your expenses: ")
+            print(expense_dict)
+        
 
-def Total_Expenses():
-    if menu == '4':
-    yada yada
+    Expense_View()
 
-Total_Expenses()
+    def Categorize_Expenses():
+        if menu == '3':
+        yada yada
 
-def Expense_Deletion():
-    if menu == '5':
-    yada yada
+    Categorize_Expenses()
 
-Expense_Deletion()
+    def Total_Expenses():
+        if menu == '4':
+        yada yada
+
+    Total_Expenses()
+
+    def Expense_Deletion():
+        if menu == '5':
+        yada yada
+
+    Expense_Deletion()
